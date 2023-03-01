2474d1edbff382f0c9487d95e1eb2257d3096227
halfawi1703 <halfawi1703@gmail.com>
2023-03-02 00:13:49 +0700

    Final commit (edit view add and edit job)

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -12,47 +12,58 @@
   </head>
   <body>
     <div class="container">
-        <div class="mb-3">
-            <a href="{{$base_url}}"> Kembali</a>
+        <div class="row">
+            <div class="col-12">
+                <a href="{{$base_url}}"> Back to list</a>
+            </div>
         </div>
-        <h2>Add Jobs</h2>
-        <!-- Content here -->
-        <form action="{{$base_url}}/store" method="POST">
-            {{ csrf_field() }}
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Name</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" name="name" placeholder="" required>
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1>Add Jobs</h1>
             </div>
-            <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Description</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="description" required></textarea>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <!-- Content here -->
+                <form action="{{$base_url}}/store" method="POST">
+                    {{ csrf_field() }}
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" name="name" placeholder="" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="description" required></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Education Level</label>
+                        <select class="form-select" aria-label="Default select example" name="education_level" required>
+                            <option value="">Open this select menu</option>
+                            <option value="SD">SD</option>
+                            <option value="SMP">SMP</option>
+                            <option value="SMA">SMA</option>
+                            <option value="S1">S1</option>
+                            <option value="S2">S2</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Start Date</label>
+                        <input type="date" class="form-control" id="exampleFormControlInput1" name="start_date" placeholder="" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">End Date</label>
+                        <input type="date" class="form-control" id="exampleFormControlInput1" name="end_date" placeholder="" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Quota</label>
+                        <input type="number" class="form-control" id="exampleFormControlInput1" name="quota" placeholder="" required>
+                    </div>
+                    <div class="d-grid gap-2">
+                        <button class="btn btn-primary" type="submit">Add Job</button>
+                    </div>
+                </form>
             </div>
-            <div class="mb-3">
-                <select class="form-select" aria-label="Default select example" name="education_level" required>
-                    <option value="">Open this select menu</option>
-                    <option value="SD">SD</option>
-                    <option value="SMP">SMP</option>
-                    <option value="SMA">SMA</option>
-                    <option value="S1">S1</option>
-                    <option value="S2">S2</option>
-                </select>
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Start Date</label>
-                <input type="date" class="form-control" id="exampleFormControlInput1" name="start_date" placeholder="" required>
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">End Date</label>
-                <input type="date" class="form-control" id="exampleFormControlInput1" name="end_date" placeholder="" required>
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Quota</label>
-                <input type="number" class="form-control" id="exampleFormControlInput1" name="quota" placeholder="" required>
-            </div>
-            <div class="d-grid gap-2">
-                <button class="btn btn-primary" type="submit">Add Job</button>
-              </div>
-        </form>
+        </div>
     </div>
     <!-- Optional JavaScript; choose one of the two! -->
 
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -12,48 +12,59 @@
   </head>
   <body>
     <div class="container">
-        <div class="mb-3">
-            <a href="{{$base_url}}"> Kembali</a>
+        <div class="row">
+            <div class="col-12">
+                <a href="{{$base_url}}"> Back to list</a>
+            </div>
         </div>
-        <h2>Add Jobs</h2>
-        <!-- Content here -->
-        <form action="{{$base_url}}/update" method="POST">
-            {{ csrf_field() }}
-            <input type="hidden" name="id" value="{{$jobs['data']['id']}}"> <br/>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Name</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" name="name" placeholder="" value="{{$jobs['data']['name']}}" required>
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1>Edit Jobs</h1>
             </div>
-            <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Description</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="description" required>{{@$jobs['data']['description'] ? $jobs['data']['description'] : ""}}</textarea>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <!-- Content here -->
+                <form action="{{$base_url}}/update" method="POST">
+                    {{ csrf_field() }}
+                    <input type="hidden" name="id" value="{{$jobs['data']['id']}}"> <br/>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" name="name" placeholder="" value="{{$jobs['data']['name']}}" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="description" required>{{@$jobs['data']['description'] ? $jobs['data']['description'] : ""}}</textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Education Level</label>
+                        <select class="form-select" aria-label="Default select example" name="education_level" required>
+                            <option value="">Open this select menu</option>
+                            <option value="SD" {{$jobs['data']['education_level'] == "SD"?"selected":""}}>SD</option>
+                            <option value="SMP" {{$jobs['data']['education_level'] == "SMP"?"selected":""}}>SMP</option>
+                            <option value="SMA" {{$jobs['data']['education_level'] == "SMA"?"selected":""}}>SMA</option>
+                            <option value="S1" {{$jobs['data']['education_level'] == "S1"?"selected":""}}>S1</option>
+                            <option value="S2" {{$jobs['data']['education_level'] == "S2"?"selected":""}}>S2</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Start Date</label>
+                        <input type="date" class="form-control" id="exampleFormControlInput1" name="start_date" placeholder="" value="{{$jobs['data']['start_date']}}" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">End Date</label>
+                        <input type="date" class="form-control" id="exampleFormControlInput1" name="end_date" placeholder="" value="{{$jobs['data']['end_date']}}" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Quota</label>
+                        <input type="number" class="form-control" id="exampleFormControlInput1" name="quota" placeholder="" value="{{$jobs['data']['quota']}}" required>
+                    </div>
+                    <div class="d-grid gap-2">
+                        <button class="btn btn-primary" type="submit">Update Job</button>
+                    </div>
+                </form>
             </div>
-            <div class="mb-3">
-                <select class="form-select" aria-label="Default select example" name="education_level" required>
-                    <option value="">Open this select menu</option>
-                    <option value="SD" {{$jobs['data']['education_level'] == "SD"?"selected":""}}>SD</option>
-                    <option value="SMP" {{$jobs['data']['education_level'] == "SMP"?"selected":""}}>SMP</option>
-                    <option value="SMA" {{$jobs['data']['education_level'] == "SMA"?"selected":""}}>SMA</option>
-                    <option value="S1" {{$jobs['data']['education_level'] == "S1"?"selected":""}}>S1</option>
-                    <option value="S2" {{$jobs['data']['education_level'] == "S2"?"selected":""}}>S2</option>
-                </select>
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Start Date</label>
-                <input type="date" class="form-control" id="exampleFormControlInput1" name="start_date" placeholder="" value="{{$jobs['data']['start_date']}}" required>
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">End Date</label>
-                <input type="date" class="form-control" id="exampleFormControlInput1" name="end_date" placeholder="" value="{{$jobs['data']['end_date']}}" required>
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Quota</label>
-                <input type="number" class="form-control" id="exampleFormControlInput1" name="quota" placeholder="" value="{{$jobs['data']['quota']}}" required>
-            </div>
-            <div class="d-grid gap-2">
-                <button class="btn btn-primary" type="submit">Update Job</button>
-              </div>
-        </form>
+        </div>
     </div>
     <!-- Optional JavaScript; choose one of the two! -->
 
